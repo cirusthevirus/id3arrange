057a574f9ff7a37ae7683d1ba9832be6c2595b60
Jakob Beckmann <beckmann_jakob@hotmail.fr>
2017-06-30 18:40:52 +0200

    Modified the script to take advantage of the os.path module for Windows compatibility. Moreover, added a feature to make all 'feat.', 'ft.' occurences in song titles and artist names uniform.

--- id3arrange.py
+++ id3arrange.py
@@ -11,43 +11,59 @@ descriptions)
 from mutagen.easyid3 import EasyID3
 import re
 import os
+import shutil
 
 invalid = re.compile('audio|premiere|original|official|lyric', re.IGNORECASE)
+feat = re.compile(' feat | feat. | ft ', re.IGNORECASE)
+feat_bis = re.compile('\(feat |\(feat. |\(ft ', re.IGNORECASE)
 
 def run():
-    try:
-        os.mkdir('./_invalid')
-    except:
-        print("_invalid directory already exists.")
-
+    # Scan files from input
+    os.chdir(source)
     files = [f for f in os.listdir('.') if f[-4:] == ".mp3"]
     tag_list = {}
 
+    try:
+        os.mkdir(os.path.join(destination, '_invalid'))
+    except:
+        print("_invalid directory already exists.")
+
     for file in files:
+        # Change file name and replace feat flag with 'ft.'
+        os.rename(file, re.sub(feat, ' ft. ', file))
+        file = re.sub(feat, ' ft. ', file)
+        os.rename(file, re.sub(feat_bis, '(ft. ', file))
+        file = re.sub(feat_bis, '(ft. ', file)
+
         tags = re.split(' - ', file)
         if re.search(invalid, file) or len(tags) != 2:
-            os.rename(file, "./_invalid/"+file)
+            os.rename(file, os.path.join(destination, "_invalid", file))
             files.remove(file)
         else:
             tag_list[file] = EasyID3(file)
-            tag_list[file]['artist'] = tags[0].strip()
-            tag_list[file]['title'] = tags[1][:-4].strip()
+
+            # Process artist name
+            artist_name = tags[0].strip()
+            tag_list[file]['artist'] = artist_name
+
+            # Process song title
+            title_name = tags[1].strip()
+            tag_list[file]['title'] = title_name
             tag_list[file].save()
+
             try:
-                os.mkdir('./'+tags[0])
+                os.mkdir(os.path.join(destination, tags[0]))
             except:
                 print("Artist folder \'%s\' already exists." % tags[0])
-            os.rename(file, "./"+tags[0]+"/"+file)
+            shutil.copy2(file, os.path.join(destination, tags[0], file))
 
 if __name__ == "__main__":
     save_dir = os.getcwd()
-
-    # Substitute the default path to which your music is downloaded
-    directory = "/Users/user/path/to/music"
-    os.chdir(directory)
-
-    # Safeguard
-    x = input("Working in: %s\nEnter 'continue' to continue.\n" % os.getcwd())
+    source = "/path/to/music/source"
+    destination = "/path/to/music/destination"
+    print("Working with source:\t\t %s" % source)
+    print("Working with destination:\t %s" % destination)
+    x = input("Enter 'continue' to continue.\n")
     if x == "continue":
         run()
     os.chdir(save_dir)
